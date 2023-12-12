99553917a08ab71144f60be279ac75a3f8d53035
Tranchoup <138502234+Tranchoup@users.noreply.github.com>
2023-12-12 16:37:28 +0100

    Add files via upload

---
+++ control_servo.ip_user_files/ip/Square_Root/Square_Root_stub.vhdl
@@ -0,0 +1,35 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2023.2 (win64) Build 4029153 Fri Oct 13 20:14:34 MDT 2023
+-- Date        : Wed Nov 29 21:26:30 2023
+-- Host        : K4des running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/Users/K4des/OneDrive/Bureau/control_servo/control_servo.srcs/sources_1/import_accelerometre/Square_Root_stub.vhdl
+-- Design      : Square_Root
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a100tcsg324-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Square_Root is
+  Port ( 
+    aclk : in STD_LOGIC;
+    s_axis_cartesian_tvalid : in STD_LOGIC;
+    s_axis_cartesian_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    m_axis_dout_tvalid : out STD_LOGIC;
+    m_axis_dout_tdata : out STD_LOGIC_VECTOR ( 15 downto 0 )
+  );
+
+end Square_Root;
+
+architecture stub of Square_Root is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "aclk,s_axis_cartesian_tvalid,s_axis_cartesian_tdata[31:0],m_axis_dout_tvalid,m_axis_dout_tdata[15:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "cordic_v6_0_20,Vivado 2023.2";
+begin
+end;
